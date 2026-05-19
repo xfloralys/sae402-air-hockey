b35434b73122414e082a5ce4ab4925ef22f76de9
Lyrae <Samuel.Doyen.Etu@univ-lemans.fr>
2026-05-19 16:05:04 +0200

    Mise en place des bases de la vue du jeu

---
+++ SAE402AirHockey/app/src/main/java/com/example/sae402airhockey/GameView.java
@@ -0,0 +1,31 @@
+package com.example.sae402airhockey;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+        // init(null, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // init(attrs, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        // init(attrs, defStyle);
+    }
+}
